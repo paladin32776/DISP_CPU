da4455ec5800b4f9a8b325028505bf4eb63d30b7
paladin32776 <31077549+paladin32776@users.noreply.github.com>
2024-09-17 13:03:59 +0200

    Major Update (Class Reunion)
    - Swapped rows/cols
    - Added default CONTROL memory content ("microcode")

--- DISP_CPU.cpp
+++ DISP_CPU.cpp
@@ -12,27 +12,27 @@ DISP_CONTROL::DISP_CONTROL()
   tft->init(); // Initialise the TFT screen
   tft->setRotation(0); // Set the rotation
   tft->fillScreen(TFT_BLACK); // Clear the screen
-  // tft->fillRect(BIT_AREA_XMIN, BIT_AREA_YMIN, BIT_AREA_XMAX-BIT_AREA_XMIN, 
+  // tft->fillRect(BIT_AREA_XMIN, BIT_AREA_YMIN, BIT_AREA_XMAX-BIT_AREA_XMIN,
   // 				BIT_AREA_YMAX-BIT_AREA_YMIN, TFT_GREEN); // Draw keypad background
   Serial.println("done.");
   // Setting touch screen calibration:
   tft->setTouch(calData);
-  // Bit memory init:
-  Serial.print("Initializing memory ... ");
-  for (int n=0; n<MEM_COLS; n++)
-  	mem[n] = 155;
+  // // Bit memory init:
+  // Serial.print("Initializing memory ... ");
+  // for (int n=0; n<MEM_COLS; n++)
+  // 	mem[n] = 155;
   Serial.println("done.");
   Serial.print("Setting up bit display ... ");
   show_mem();
   Serial.println("done.");
 }
 
-void DISP_CONTROL::show_col(unsigned int col)
+void DISP_CONTROL::show_row(unsigned int row)
 {
   unsigned int bgcolor;
   tft->setFreeFont(BIT_FONT);
   tft->setTextSize(1);
-  if (col==active_col)
+  if (row==active_row)
     {
       tft->setTextColor(TFT_BLACK);
       bgcolor = TFT_WHITE;
@@ -40,23 +40,23 @@ void DISP_CONTROL::show_col(unsigned int col)
     else
     {
       tft->setTextColor(TFT_WHITE);
-      bgcolor = bg[col];
+      bgcolor = bg[row];
     }
-    tft->fillRect(BIT_AREA_XMIN+col*BIT_AREA_DX, BIT_AREA_YMIN,
-                  BIT_AREA_DX, BIT_AREA_YMAX-BIT_AREA_YMIN,
+    tft->fillRect(BIT_AREA_XMIN, BIT_AREA_YMIN+row*BIT_AREA_DY,
+                  BIT_AREA_XMAX-BIT_AREA_XMIN, BIT_AREA_DY,
                   bgcolor);
-    for (int row=0; row<MEM_ROWS; row++)
-      tft->drawNumber(bitRead(mem[col],row), 
+    for (int col=0; col<MEM_COLS; col++)
+      tft->drawNumber(bitRead(mem[row],MEM_COLS-col-1),
                       BIT_TEXT_XMIN+col*BIT_AREA_DX, BIT_TEXT_YMIN+row*BIT_AREA_DY);
 }
 
 void DISP_CONTROL::show_mem()
 {
 
-  tft->fillRect(BIT_AREA_XMIN, BIT_AREA_YMIN, BIT_AREA_XMAX-BIT_AREA_XMIN, 
+  tft->fillRect(BIT_AREA_XMIN, BIT_AREA_YMIN, BIT_AREA_XMAX-BIT_AREA_XMIN,
          BIT_AREA_YMAX-BIT_AREA_YMIN, TFT_BLACK); // Draw keypad background
-  for (int col=0; col<MEM_COLS; col++)
-    show_col(col);
+  for (int row=0; row<MEM_ROWS; row++)
+    show_row(row);
 }
 
 void DISP_CONTROL::check()
@@ -69,50 +69,60 @@ void DISP_CONTROL::check()
   {
     unsigned int col = int((tx - BIT_TEXT_XMIN) / BIT_AREA_DX);
     unsigned int row = int((ty - BIT_TEXT_YMIN) / BIT_AREA_DY);
-    toggleBit(col, row);
+    toggleBit(row, col);
     Serial.printf("row = %d   col = %d\n", row, col);
   }
 
 }
 
-void DISP_CONTROL::setcol(unsigned int col)
+void DISP_CONTROL::setrow(unsigned int row)
 {
-  unsigned int old_col = active_col;
-  active_col = col;
-  show_col(old_col);
-  show_col(active_col);
+  if (row==active_row)
+    return;
+  unsigned int old_row = active_row;
+  active_row = row;
+  show_row(old_row);
+  show_row(active_row);
 }
 
-unsigned int DISP_CONTROL::get_ctrl(unsigned int col)
+unsigned int DISP_CONTROL::reverse6bit(unsigned int x)
 {
-  return ((mem[col] & 0b0011111100) >> 2);
+	unsigned int xout = 0;
+	for (int n=0; n<6; n++)
+		xout += (bitRead(x,n)<<(5-n));
+	return xout;
 }
 
-unsigned int DISP_CONTROL::get_cset(unsigned int col)
+unsigned int DISP_CONTROL::get_ctrl(unsigned int row)
 {
-  return ((mem[col] & 0b0000000010) >> 1);
+  return reverse6bit((mem[row] & 0b1111110000) >> 4);
 }
 
-unsigned int DISP_CONTROL::get_creset(unsigned int col)
+unsigned int DISP_CONTROL::get_cset(unsigned int row)
 {
-  return ((mem[col] & 0b0000000001));
+  return ((mem[row] & 0b0000000100) >> 2);
 }
 
-unsigned int DISP_CONTROL::get_pcount(unsigned int col)
+unsigned int DISP_CONTROL::get_creset(unsigned int row)
 {
-  return ((mem[col] & 0b1000000000) >> 9);
+  return ((mem[row] & 0b0000001000) >> 3);
 }
 
-unsigned int DISP_CONTROL::get_pset(unsigned int col)
+unsigned int DISP_CONTROL::get_pcount(unsigned int row)
 {
-  return ((mem[col] & 0b0100000000) >> 8);
+  return ((mem[row] & 0b0000000010) >> 1);
 }
 
-void DISP_CONTROL::toggleBit(unsigned int col, unsigned int row)
+unsigned int DISP_CONTROL::get_pset(unsigned int row)
+{
+  return ((mem[row] & 0b0000000001));
+}
+
+void DISP_CONTROL::toggleBit(unsigned int row, unsigned int col)
 {
   // bitWrite(mem[col], row, !bitRead(mem[col], row));
   // show_mem();
-  setcol(col);
+  setrow(row);
 }
 
 // unsigned char LED_CONTROL::reverse4bit(unsigned char x)
@@ -123,12 +133,12 @@ void DISP_CONTROL::toggleBit(unsigned int col, unsigned int row)
 // 	return xout;
 // }
 
-// void LED_CONTROL::update(unsigned char bus, unsigned char ctrl, unsigned char counter, 
+// void LED_CONTROL::update(unsigned char bus, unsigned char ctrl, unsigned char counter,
 // 			unsigned char ccount, unsigned char cset, unsigned char creset,
 // 			unsigned char pcount, unsigned char pset)
 // {
 // 	unsigned short leddata1 = ((ccount & 1)<<3)
-// 	                        + ((counter & 0x0F)<<4) 
+// 	                        + ((counter & 0x0F)<<4)
 // 	                        + ((creset & 1)<<10)
 // 	                        + ((cset & 1)<<11)
 // 	                        + ((reverse4bit(bus) & 0x0F)<<12);
--- DISP_CPU.h
+++ DISP_CPU.h
@@ -9,66 +9,56 @@
 #define CTRL_BIT_RC 4
 #define CTRL_BIT_G2 5
 
-#define MEM_COLS 16
-#define MEM_ROWS 10
+#define MEM_COLS 10
+#define MEM_ROWS 16
 
 #define BIT_AREA_XMIN 0
 #define BIT_AREA_XMAX 240
-#define BIT_AREA_YMIN 80
+#define BIT_AREA_YMIN 0
 #define BIT_AREA_YMAX 320
-#define BIT_AREA_DX 15
-#define BIT_AREA_DY 24
+#define BIT_AREA_DX 24
+#define BIT_AREA_DY 20
 #define BIT_TEXT_XMIN 2
-#define BIT_TEXT_YMIN 85
+#define BIT_TEXT_YMIN 5
 
 #define BIT_TEXT_SIZE 1   // Font size multiplier
 #define BIT_FONT &FreeSansBold9pt7b    // Key label font 2
 
 #define TFT_DARKYELLOW 0x9CC0
 
-// LED connections on ALU board:
-// =============================
-// led1_0..led1_3 : bus 0..3
-// led1_4..led1_7 : ra 0..3
-// led1_8..led1_11 : rb 3..0
-// led1_12 : ePM
-// led1_13 : eRA
-// led1_14 : eRB
-// led1_15 : eG1
-
-// led2_0..led2_3 : rc 3..0
-// led2_4 : eRC
-// led2_5 : neg
-// led2_6 : overflow
-// led2_7 : eG2
-
 
 class DISP_CONTROL
 {
 	private:
 		TFT_eSPI *tft;
-		TFT_eSPI_Button *membits[MEM_COLS][MEM_ROWS];
-		unsigned int mem[MEM_COLS] = {0b1};
+		TFT_eSPI_Button *membits[MEM_ROWS][MEM_COLS];
+		uint16_t mem[MEM_ROWS] = {0b0000000100, 0b1010000010, 0b1000000000,
+															0b0000001010, 0b1101000010, 0b1001100000,
+														  0b0001010000, 0b0011010000, 0b0001011010,
+														  0b1100000010, 0b1000100000, 0b0000010000,
+														  0b0010010000, 0b0000011010, 0b0100000010,
+														  0b0000001001};
 		// Touch screen calibration data:
 		uint16_t calData[5] = {203, 3657, 350, 3491, 4};
-		unsigned int bg[16] = {	TFT_RED, TFT_RED, 
-							   	TFT_DARKYELLOW, TFT_DARKYELLOW, TFT_DARKYELLOW, 
-							   	TFT_DARKGREEN, TFT_DARKGREEN, TFT_DARKGREEN, TFT_DARKGREEN, TFT_DARKGREEN, 
-							   	TFT_BLUE, TFT_BLUE, TFT_BLUE, TFT_BLUE, TFT_BLUE, 
-							   	TFT_PURPLE};
-		unsigned int active_col = 0;
-		void show_col(unsigned int col);
+		unsigned int bg[16] = {	TFT_PURPLE,
+									TFT_BLUE, TFT_BLUE, TFT_BLUE,
+									TFT_DARKGREEN, TFT_DARKGREEN, TFT_DARKGREEN, TFT_DARKGREEN, TFT_DARKGREEN,
+							   	TFT_DARKYELLOW, TFT_DARKYELLOW, TFT_DARKYELLOW, TFT_DARKYELLOW, TFT_DARKYELLOW,
+							   	TFT_RED, TFT_RED};
+		unsigned int active_row = 0;
+		void show_row(unsigned int row);
 		void show_mem();
+		unsigned int reverse6bit(unsigned int x);
 	public:
 		DISP_CONTROL();
 		void check();
-		void setcol(unsigned int col);
-		unsigned int get_ctrl(unsigned int col);
-		unsigned int get_cset(unsigned int col);
-		unsigned int get_creset(unsigned int col);
-		unsigned int get_pcount(unsigned int col);
-		unsigned int get_pset(unsigned int col);
-		void toggleBit(unsigned int col, unsigned int row);
+		void setrow(unsigned int row);
+		unsigned int get_ctrl(unsigned int row);
+		unsigned int get_cset(unsigned int row);
+		unsigned int get_creset(unsigned int row);
+		unsigned int get_pcount(unsigned int row);
+		unsigned int get_pset(unsigned int row);
+		void toggleBit(unsigned int row, unsigned int col);
 		// void update(unsigned char bus, unsigned char ctrl, unsigned char counter,
 		// 	unsigned char ccount, unsigned char cset, unsigned char creset,
 		// 	unsigned char pcount, unsigned char pset);
